100026b5678b21e029422ac13311c1378ad51058
nickoolaz <nicolasmarceloaraujo@gmail.com>
2022-11-13 19:56:06 -0300

    Criação da rota usuários

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\ProdutosController;
+use App\Http\Controllers\UsuariosController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('produtos', [ProdutosController::class, 'index']);
@@ -11,6 +12,14 @@ Route::get('produtos/excluir/{id}', [ProdutosController::class, 'destroy']);
 Route::get('produtos/{id}', [ProdutosController::class, 'show']);
 Route::post('produtos/gravar/{id}', [ProdutosController::class, 'update']);
 Route::post('produtos/salvar', [ProdutosController::class, 'store']);
+
+Route::get('usuarios', [UsuariosController::class, 'index']);
+Route::get('usuarios/novo', [usuariosController::class, 'create']);
+Route::get('usuarios/editar/{id}', [usuariosController::class, 'edit']);
+Route::get('usuarios/excluir/{id}', [usuariosController::class, 'destroy']);
+Route::get('usuarios/{id}', [usuariosController::class, 'show']);
+Route::post('usuarios/gravar/{id}', [usuariosController::class, 'update']);
+Route::post('usuarios/salvar', [usuariosController::class, 'store']);
 
 Route::get('home', [HomeController::class, 'index']);
 Route::get('users', [HomeController::class, 'users']);
